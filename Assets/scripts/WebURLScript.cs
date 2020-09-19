22b30a436e3f25276e4b69d0086301f8a49bb631
Sayna Parida <saynaparida33@gmail.com>
2020-09-19 22:24:33 +0530

    Assests added

---
+++ Assets/scripts/WebURLScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebURLScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void btnOne()
+    {
+        Application.OpenURL("https://www.facebook.com/profile.php?id=100004305275178");
+    }
+
+    public void btnTwo()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/ravi-kumar-93276117b");
+    }
+
+    public void btnthree()
+    {
+        Application.OpenURL("https://www.instagram.com/ravi27280/");
+    }
+
+    public void btnfour()
+    {
+        Application.OpenURL("www.google.com");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/scripts/rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+  
+{
+    Vector3 movments;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movments = new Vector3(0,1,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            transform.Rotate(movments);
+    }
+}
